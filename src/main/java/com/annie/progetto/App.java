83b4a3183e34d9ac185937385f5ea5f2dfed45bf
Annie <ana_niagu@ymail.com>
2023-02-17 16:16:18 +0100

    INITIAL COMMIT

---
+++ src/main/java/com/annie/progetto/App.java
@@ -0,0 +1,13 @@
+package com.annie.progetto;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
